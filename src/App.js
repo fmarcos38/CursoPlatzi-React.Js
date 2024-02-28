673a8d4e6a1567fa38f90e07e81945378fe6228d
fmarcos38 <fmarcos_23@hotmail.com>
2024-02-28 15:05:52 -0300

    localStorage

--- src/App.js
+++ src/App.js
@@ -7,23 +7,59 @@ import React, { useState } from 'react';
 
 //NOTA: los estados se manejan en el componente padre, y se pasan por props a los hijos
 //creo array d areas
-const arrTodos = [
+/* const arrTodos = [
   { text: 'Cortar cebolla', completed: true },
   { text: 'Tomar el curso de intro a React', completed: false },
   { text: 'Llorar con la llorona', completed: false },
   { text: 'Decidir de q mierda laburar', completed: true }
-];
+]; */
 
 
 function App() {
+
+  //Manejo del localStorage
+  const localStorageTodos = JSON.parse(localStorage.getItem('todos')); //este va a ser mi nuevo estado
+  let todosInicial; //variable para el estado inicial con los todos del localStorage
+  if(!localStorageTodos) { //si no hay nada en el localStorage creo un array vacio
+    localStorage.setItem('todos', JSON.stringify([]));
+  }else{
+    todosInicial = localStorageTodos;
+  }
+  
+  //funcion para actualizar el localStorage y el estado
+  const saveTodos = (newTodos) => {
+    const stringTodos = JSON.stringify(newTodos);
+    localStorage.setItem('todos', stringTodos);
+    setTodos(newTodos);
+  };
+  
   //estado para los todos
-  const [todos, setTodos] = useState(arrTodos);
+  const [todos, setTodos] = useState(todosInicial);
   //estado para el SEARCH
-  const [search, setSearch] = useState(''); console.log(search);
+  const [search, setSearch] = useState('');
   
   //variables para el total y completados
   const completedTodos = todos.filter( todo => !!todo.completed ).length;
   const totalTodos = todos.length;
+  
+  //filtro los todos segun el search
+  const searchToDos  = todos.filter(todo => todo.text.toLowerCase().includes(search.toLowerCase()));//paso todo a minus y comparo
+
+  //funcion para marcar como completado
+  const handlerComplete = (text) => {
+    const todoIndex = todos.findIndex( todo => todo.text === text );
+    const newTodos = [...todos];
+    newTodos[todoIndex].completed = !newTodos[todoIndex].completed;
+    saveTodos(newTodos); //actualizo localStorage y estado
+  };
+  //funcion para borrar
+  const handlerDelete = (text) => {
+    const todoIndex = todos.findIndex( todo => todo.text === text );
+    const newTodos = [...todos];
+    newTodos.splice(todoIndex, 1);
+    saveTodos(newTodos); //actualizo localStorage y estado
+  };
+
 
   return (
     /* uilizo React.Fragment en vez de un div */
@@ -34,8 +70,14 @@ function App() {
       <TodoList>
         {/* todo lo q se desarrolle dentro del comp TodoList --> react lo pasa por props en children */}
         {
-          arrTodos.map(todo => (
-            <TodoItem key={todo.text} text={todo.text} completed={todo.completed} />
+          searchToDos.map(todo => (
+            <TodoItem 
+              key={todo.text} 
+              text={todo.text} 
+              completed={todo.completed} 
+              handlerComplete={handlerComplete}
+              handlerDelete={handlerDelete}
+            />
           ))
         }
       </TodoList>
--- src/components/todoItem.js
+++ src/components/todoItem.js
@@ -1,16 +1,24 @@
 import './todoItem.css'; 
 
-function TodoItem(props) { 
+function TodoItem({ text, completed, handlerComplete, handlerDelete}) { 
     return (
         <li className="TodoItem">
             {/* utilizo clases dinamicas SI viene TRUE(osea completada) &&(entonces)*/}
-            <span className={`Icon Icon-check ${props.completed && "Icon-check--active"}`}> 
-                V
+            <span 
+                className={`Icon Icon-check ${completed && "Icon-check--active"}`}
+                onClick={() => handlerComplete(text)}
+            > 
+                ✔
             </span>
-            <p className={`TodoItem-p ${props.completed && "TodoItem-p--complete"}`}>
-                {props.text}
+            <p className={`TodoItem-p ${completed && "TodoItem-p--complete"}`}>
+                {text}
             </p>
-            <span className="Icon Icon-delete">X</span>
+            <span 
+                className="Icon Icon-delete"
+                onClick={() => handlerDelete(text)}
+            >
+                X
+            </span>
         </li>
     ); 
 }
